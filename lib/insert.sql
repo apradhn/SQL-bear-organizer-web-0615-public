d96c37e49f615b6bba8db42f0e9b4b270e3235ce
apradhn <abhijit.pradhan6@gmail.com>
2015-06-17 09:54:20 -0400

    all done

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES 
+("Mr. Chocoloate", 9, "M", "Brown", "Bashful", 1),
+("Rowdy", 12, "M", "Black", "Grumpy", 0),
+("Tabitha", 11, "F", "White", "Sleepy", 1),
+("Sergeant Brown", 10, "M", "Brown", "Disciplined", 1),
+("Melissa", 9, "F", "Black", "Agreeable", 1),
+("Grinch", 11, "M", "Dark Brown", "Spiteful", 0),
+("Wendy", 12, "F", "Dark Brown", "Space Cadet", 1);
+INSERT INTO bears(age, gender, color, temperament, alive) VALUES
+(13, "M", "Dark Brown", "Idealistic", 1);
